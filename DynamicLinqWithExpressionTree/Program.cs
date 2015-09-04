b09792970ee3d3544732f93ad1f7dc19c39b62b2
Chiêu Minh Tâm <tam.chieu@newoceaninfosys.com>
2015-09-04 17:25:24 +0700

    Add AdventureWorks 2014

---
+++ DynamicLinqWithExpressionTree/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynamicLinqWithExpressionTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # DynamicLinQWithExpressionTree
+Purpose for this project is to study about Expression Tree and use Dynamic Data
+Database: I use AdventureWork 2014. You can download from here: http://msftdbprodsamples.codeplex.com/
